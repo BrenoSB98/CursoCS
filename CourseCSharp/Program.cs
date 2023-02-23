828d19c6fcddf60ba444137e9a994367b6facd83
breno <brenosilvabraido1998@gmail.com>
2023-02-23 13:53:26 -0300

    Projeto Iniciado

---
+++ CourseCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CourseCSharp
+{
+    class Program {
+        static void Main(string[] args){
+            int n = int.Parse(Console.ReadLine());
+            System.Console.WriteLine(n);
+        }
+    }
+}
